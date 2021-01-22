9cb761b697d29fa46d1091c8c4c7084a4c9dd7d1
Urfeen Jus <urfeen@gmail.com>
2021-01-22 17:19:13 +0300

    projects

--- js/script.js
+++ js/script.js
@@ -34,3 +34,37 @@
   }
 })();
 
+$(document).ready(function () {
+  $(".projects__slider").slick({
+    appendArrows: $(".projects__slider-controll"),
+    slidesToShow: 3,
+    infinite: false,
+    slidesToShow: 1,
+    speed: 400,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
