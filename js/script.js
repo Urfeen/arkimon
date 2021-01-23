8e6a5c01794be8a201f31bc3c116e46856249722
Urfeen Jus <urfeen@gmail.com>
2021-01-23 17:28:20 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+{
+  //animation
+  const animItems = document.querySelectorAll("._anim-items");
+  if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll)
+    function animOnScroll() {
+      for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offSet(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add("_active");
+        } else {
+          if (!animItem.classList.contains("_anim-no-hide")) {
+            animItem.classList.remove("_active");
+          }
+        }
+      }
+    }
+    function offSet(el) {
+      const rect = el.getBoundingClientRect();
+      const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+    setTimeout(() => {
+      animOnScroll();
+    }, 500);
+  }
+}
+
 (function burger() {
   const burger = document.querySelector(".header__burger");
   burger.addEventListener("click", toggleBurger);
@@ -38,7 +76,7 @@ $(document).ready(function () {
   $(".projects__slider").slick({
     appendArrows: $(".projects__slider-control"),
     slidesToShow: 3,
-    infinite: false,
+    infinite: true,
     speed: 400,
     centerMode: true,
     variableWidth: true,
@@ -67,13 +105,13 @@ $(document).ready(function () {
     ]
   });
 
-
   $(".testimonial__slider").slick({
     appendArrows: $(".testimonial__slider-control"),
     infinite: false,
     slidesToShow: 1,
     speed: 400,
     asNavFor: '.testimonial__current-avatar',
+    draggable: false,
   });
   $(".testimonial__current-avatar").slick({
     infinite: false,
